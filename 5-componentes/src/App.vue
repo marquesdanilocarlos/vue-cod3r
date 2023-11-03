d2a77e2a43645b4f81e413375f12c07d47374b64
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-03 00:00:00 -0300

    Aula 45 - Utilização de componentes em um projeto

---
+++ 5-componentes/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <Contador></Contador>
+  </div>
+</template>
---
+++ 5-componentes/src/components/Contador.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="contador">
+    <span>{{contador}}</span>
+    <br>
+    <button @click="add">Add</button>
+    <button @click="sub">Sub</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contador",
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    add() {
+      this.contador++;
+    },
+    sub() {
+      this.contador--;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>

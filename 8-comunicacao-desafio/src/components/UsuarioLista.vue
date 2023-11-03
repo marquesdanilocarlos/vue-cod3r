e22a17a4ea35eaa8cca475b5201d999d4c22ff9c
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-03 15:39:47 -0300

    Aula 57 - Desafio de comunicação entre componentes

---
+++ 8-comunicacao-desafio/src/components/UsuarioLista.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="usuario-lista">
+    <table>
+      <thead>
+      <tr>
+        <th>#ID</th>
+        <th>Nome</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="usuario in usuarios" :key="usuario.id" @click="selectUser(usuario)">
+        <td>{{ usuario.id }}</td>
+        <td>{{ usuario.nome }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import EventBus from "@/event-bus";
+export default {
+  props: {usuarios: Array},
+  data() {
+    return {
+      selectedUser: null
+    }
+  },
+  methods: {
+    selectUser(user) {
+      this.selectedUser = user;
+      EventBus.$emit('isSelected', this.selectedUser);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.usuario-lista {
+  flex: 1;
+  display: flex;
+}
+
+table {
+  flex: 1;
+  border-spacing: 0px;
+  border-collapse: separate;
+  border-top: 1px solid #CCC;
+}
+
+
+th, td {
+  padding: 15px;
+  border-bottom: 1px solid #CCC;
+}
+
+tbody tr:hover {
+  background: rgba(0, 0, 0, 0.1);
+}
+</style>

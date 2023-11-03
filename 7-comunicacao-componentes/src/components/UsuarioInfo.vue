6316fbaeb60bac03657e907af6326707661a3f35
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-03 12:13:41 -0300

    Aula 50 - Manipulação de props no componente filho

--- 7-comunicacao-componentes/src/components/UsuarioInfo.vue
+++ 7-comunicacao-componentes/src/components/UsuarioInfo.vue
@@ -1,21 +1,35 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-      <p>Nome do Usuário: <strong>{{nome}}</strong></p>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>Nome do Usuário: <strong>{{ nome }}</strong></p>
+    <p>Nome do Usuário invertido: <strong>{{ nomeAlternativo }}</strong></p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['nome']
+  props: ['nome'],
+  mounted() {
+    this.inverterNome();
+  },
+  data(){
+    return {
+      nomeAlternativo: ''
+    }
+  },
+  methods: {
+    inverterNome() {
+      this.nomeAlternativo = this.nome.split('').reverse().join('');
+    }
+  }
 }
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>

ceab42b72b2829f240d38fab23d1772a9c9852de
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-13 23:34:33 -0300

    Aula 77 - Desafio de formulários e v-model

---
+++ 12-desafio-formularios/src/components/NomeCompleto.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input type="text" @input="setNome" :value="value.nome">
+    </Rotulo>
+    <Rotulo nome="Sobrenome">
+      <input type="text" @input="setSobreNome" :value="value.sobreNome">
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from "@/components/Rotulo.vue";
+
+export default {
+  name: "NomeCompleto",
+  components: {Rotulo},
+  props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    setNome(e){
+      this.$emit('input', {nome: e.target.value, sobreNome: this.value.sobreNome});
+    },
+    setSobreNome(e){
+      this.$emit('input', {nome: this.value.nome, sobreNome: e.target.value});
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

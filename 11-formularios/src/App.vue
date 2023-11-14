9df79370b4cb9eff5ddd1c67dff0cdf8384d7ffa
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-13 21:41:46 -0300

    Aula 74 - V-model com radio button (valor em array)

--- 11-formularios/src/App.vue
+++ 11-formularios/src/App.vue
@@ -21,9 +21,9 @@
           <span><input type="checkbox" value="intermitente" v-model="caracteristicas"> Intermitente</span>
         </Rotulo>
         <Rotulo nome="Qual produto?">
-          <span class="mr-4"><input type="radio"> Web</span>
-          <span class="mr-4"><input type="radio"> Mobile</span>
-          <span><input type="radio"> Outro</span>
+          <span class="mr-4"><input type="radio" value="web" v-model="produto"> Web</span>
+          <span class="mr-4"><input type="radio" value="mobile" v-model="produto"> Mobile</span>
+          <span><input type="radio" value="outro" v-model="produto"> Outro</span>
         </Rotulo>
         <Rotulo nome="Prioridade">
           <select name="" id="">
@@ -53,12 +53,12 @@
         <Rotulo nome="Marque as Opções">
           <span>
             <ul>
-              <li v-for="caracteristica in caracteristicas" :key="caracteristica">{{ caracteristica}}</li>
+              <li v-for="caracteristica in caracteristicas" :key="caracteristica">{{ caracteristica }}</li>
               </ul>
           </span>
         </Rotulo>
         <Rotulo nome="Qual produto?">
-          <span>???</span>
+          <span>{{ produto }}</span>
         </Rotulo>
         <Rotulo nome="Prioridade">
           <span>???</span>
@@ -86,7 +86,8 @@ export default {
         idade: 32,
       },
       mensagem: '',
-      caracteristicas: []
+      caracteristicas: [],
+      produto: 'web'
     }
   },
   computed: {

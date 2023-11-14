c22a2733faaabd1243682677cc5e4c8cd3b5cde6
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-14 14:57:46 -0300

    Aula 86 - Criação de filtro

---
+++ 15-filtros-mixins/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Filtros & Mixins</h1>
+    <p>{{ cpf | cpf }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cpf: '03321844125'
+    }
+  },
+  filters: {
+    cpf(cpf) {
+      const arr = cpf.split('');
+      arr.splice(3, 0, '.');
+      arr.splice(7, 0, '.');
+      arr.splice(11, 0, '-');
+      return arr.join('');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
+}
+</style>

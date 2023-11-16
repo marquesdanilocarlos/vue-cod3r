057e1e94faf662e78a425964d529fd3f33fd0feb
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-16 13:02:02 -0300

    Aula 105 - Adição de Headers

--- 18-requisicoes-http-axios/src/plugins/axios.js
+++ 18-requisicoes-http-axios/src/plugins/axios.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://curso-vue-2c165-default-rtdb.firebaseio.com/';
+//axios.defaults.baseURL = '';
+//axios.defaults.headers.common['Authorization'] = 'abc123';
+//axios.defaults.headers.get['Accepts'] = 'application.json'
 
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = axios;
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://curso-vue-2c165-default-rtdb.firebaseio.com/',
+            headers: {
+                "Authorization": "abc123"
+            }
+        });
         Vue.prototype.$http.interceptors.request.use(config => {
                 //console.log(config.method);
                 return config;

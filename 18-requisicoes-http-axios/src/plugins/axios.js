48d98f951142571ae0bcfb35599be6a4c5cca187
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-16 12:09:28 -0300

    Aula 103 - Interceptação de requisições

--- 18-requisicoes-http-axios/src/plugins/axios.js
+++ 18-requisicoes-http-axios/src/plugins/axios.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://curso-vue-2c165-default-rtdb.firebaseio.com/';
+
 Vue.use({
-    install(Vue){
-        Vue.prototype.$http = axios.create({
-            baseUrl: 'https://curso-vue-2c165-default-rtdb.firebaseio.com/'
+    install(Vue) {
+        Vue.prototype.$http = axios;
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method);
+            return config;
         });
     }
 });

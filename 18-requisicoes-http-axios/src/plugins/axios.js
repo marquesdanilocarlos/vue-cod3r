bccfb7399b1a355b2e151fc568c08a195cb92efb
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-16 12:15:45 -0300

    Aula 104 - Interceptação de respostas

--- 18-requisicoes-http-axios/src/plugins/axios.js
+++ 18-requisicoes-http-axios/src/plugins/axios.js
@@ -7,8 +7,17 @@ Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios;
         Vue.prototype.$http.interceptors.request.use(config => {
-            console.log(config.method);
-            return config;
-        });
+                //console.log(config.method);
+                return config;
+            },
+            error => Promise.reject(error));
+        Vue.prototype.$http.interceptors.response.use(response => {
+            const arr = [];
+            for (let chave in response.data) {
+                arr.push({id: chave, ...response.data[chave]})
+            }
+            response.data = arr;
+            return response;
+        }, error => Promise.reject(error));
     }
 });

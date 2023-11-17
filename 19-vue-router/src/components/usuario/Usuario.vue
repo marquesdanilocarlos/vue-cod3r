6b0a64bb0a943a6d23e673ca5231d31c4b29ef5b
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-17 15:06:07 -0300

    Aula 111 - Métodos de navegação do Vue Router

--- 19-vue-router/src/components/usuario/Usuario.vue
+++ 19-vue-router/src/components/usuario/Usuario.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="usuario">
     <h2>Usuário</h2>
+    <button sucesso @click="inicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Usuario"
+  name: "Usuario",
+  methods: {
+    inicio() {
+      this.$router.push('/');
+    }
+  }
 }
 </script>
 

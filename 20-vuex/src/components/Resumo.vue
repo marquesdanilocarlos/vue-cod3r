435a7753bd0ca1a147a07461bc760b9cf3090b91
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-24 17:00:56 -0300

    Aula 127 - Criação de getters na Store

--- 20-vuex/src/components/Resumo.vue
+++ 20-vuex/src/components/Resumo.vue
@@ -12,11 +12,10 @@
 export default {
   computed: {
     total() {
-      return this.produtos.map(p => p.quantidade * p.preco)
-          .reduce((total, atual) => total + atual, 0)
+      return this.$store.getters.valorTotal;
     },
     produtos() {
-      return this.$store.state.produtos
+      return this.$store.state.produtos;
     }
   }
 }

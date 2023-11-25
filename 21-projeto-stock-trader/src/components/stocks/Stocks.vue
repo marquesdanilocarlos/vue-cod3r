58a67d9cff63ff1f8701774f25b69aa3c824a981
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-11-25 17:46:57 -0300

    Aula 139 - Componente de ações

--- 21-projeto-stock-trader/src/components/stocks/Stocks.vue
+++ 21-projeto-stock-trader/src/components/stocks/Stocks.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" />
+  </v-layout>
 </template>
 
 <script>
+import Stock from "@/components/stocks/Stock.vue";
+
 export default {
-  name: "Stocks"
+  name: "Stocks",
+  components: {Stock},
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 200},
+        {id: 3, name: 'Apple', price: 320},
+        {id: 4, name: 'Twitter', price: 83},
+      ]
+    }
+  }
 }
 </script>
 

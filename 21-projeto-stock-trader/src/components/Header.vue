e5f315e875f53658ac15286a3efcd57770a0b147
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-12-05 00:41:33 -0300

    Aula 144 - Exibição de saldo nos componentes Home e Header

--- 21-projeto-stock-trader/src/components/Header.vue
+++ 21-projeto-stock-trader/src/components/Header.vue
@@ -25,13 +25,25 @@
           </v-list-tile>
         </v-list>
       </v-menu>
+      <v-layout align-center>
+        <span class="text-uppercase grey--text text--darken-2">
+          Saldo: {{ funds }}
+        </span>
+      </v-layout>
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
-  name: "Header"
+  name: "Header",
+  computed: {
+    ...mapGetters([
+      'funds'
+    ])
+  }
 }
 </script>
 
